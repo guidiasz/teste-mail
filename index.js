50b7bd63d429230435123def547b1e78146af96f
Guilherme Dias <gui.diasz@hotmail.com>
2022-04-10 00:27:25 -0300

    Update index.js

--- index.js
+++ index.js
@@ -18,3 +18,14 @@ app.post('/incoming_mails', function(req, res) {
 app.listen(PORT, function() {
 	console.log(`server started ${PORT}`);
 });
+
+const fs = require('fs');
+  
+let data = "This is a file containing a collection"
+           + " of programming languages.\n"
+ + "1. C\n2. C++\n3. Python";
+  
+fs.writeFileSync("programming.txt", data);
+console.log("File written successfully\n");
+console.log("The written has the following contents:");
+console.log(fs.readFileSync("programming.txt", "utf8"));
